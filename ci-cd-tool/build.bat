4baa078fd4959cd57420d105dec1f7beacbb1fb4
hari_08 <97215411+hari06021999@users.noreply.github.com>
2022-11-15 20:15:18 +0530

    Update build.bat

--- ci-cd-tool/build.bat
+++ ci-cd-tool/build.bat
@@ -18,9 +18,9 @@ cd "%build_dir%"
 
 set PATH=C:\ST\STM32CubeIDE_1.10.1\STM32CubeIDE\plugins\com.st.stm32cube.ide.mcu.externaltools.gnu-tools-for-stm32.10.3-2021.10.win32_1.0.0.202111181127\tools\bin;C:\ST\STM32CubeIDE_1.10.1\STM32CubeIDE\plugins\com.st.stm32cube.ide.mcu.externaltools.make.win32_2.0.100.202202231230\tools\bin;C:\ST\STM32CubeIDE_1.10.1\STM32CubeIDE\plugins\com.st.stm32cube.ide.jre.win64_2.2.0.202203101208\jre\bin\client;C:\ST\STM32CubeIDE_1.10.1\STM32CubeIDE\plugins\com.st.stm32cube.ide.jre.win64_2.2.0.202203101208\jre\bin;C:\Windows\System32;C:\Windows;C:\Windows\System32\wbem;C:\Windows\System32\WindowsPowerShell\v1.0;
 
-set compiler_prefix=arm-none-eabi-
+set compiler_prefix=arm-none-eabi-gcc
 
-set "version_file=..\Core\Src\main.c"
+set "version_file=..\Core\Src\main"
 
 if [%BUILD_TAG%]==[] goto :do_make
 
