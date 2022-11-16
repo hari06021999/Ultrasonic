d08a56eb7bb28bf0d8e83be65e293004e014a230
hari_08 <97215411+hari06021999@users.noreply.github.com>
2022-11-16 18:28:13 +0530

    Create flashi.bat

---
+++ ci-cd-tool/flashi.bat
@@ -0,0 +1,13 @@
+rem @echo off
+set "usage=usage: flashi [{Debug|Release}]"
+
+setlocal
+
+set "build_type=Debug"
+if not [%1]==[] set "build_type=%1"
+
+set "ws_root=C:\Users\harip\STM32CubeIDE\workspace_1.10.1\Ultrasonic"
+set "sn=066BFF575550897767162155"
+set "image_file=%ws_root%\%build_type%\Ultrasonic.bin"
+
+"%ws_root%\ci-cd-tool\flash.bat" %sn% "%image_file%"
