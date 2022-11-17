5c5a7610cc3ac26c4862c8665c3eb70cdf0aa729
hari_08 <97215411+hari06021999@users.noreply.github.com>
2022-11-17 09:07:44 +0530

    Update flashi.bat

--- ci-cd-tool/flashi.bat
+++ ci-cd-tool/flashi.bat
@@ -7,7 +7,7 @@ set "build_type=Debug"
 if not [%1]==[] set "build_type=%1"
 
 set "ws_root=C:\Users\harip\STM32CubeIDE\workspace_1.10.1\Ultrasonic"
-set "sn=066BFF575550897767162155"
+set "sn=066EFF485688494867113960"
 set "image_file=%ws_root%\%build_type%\"
 
 "%ws_root%\ci-cd-tool\flash.bat" %sn% "%image_file%"
